3bb540c311708c8bb598e9a9f386bc9d0b9e3fd9
Camilla Gustafsson <camie-89@hotmail.com>
2023-10-12 18:01:42 +0200

    navbar

---
+++ frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../Navigation/Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+    <Navbar/> 
+    <div>
+      HomePage
+    </div>
+    </>
+  )
+}
+
+export default HomePage
