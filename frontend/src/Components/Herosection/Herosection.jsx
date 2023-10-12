6e8c51473a43399871b23a724880cbc610211e51
Camilla Gustafsson <camie-89@hotmail.com>
2023-10-12 22:42:28 +0200

    features

--- frontend/src/Components/Herosection/Herosection.jsx
+++ frontend/src/Components/Herosection/Herosection.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import heroImage from '../../images/heroImage.jpg';
 import Stars from '../../images/stars.png';
+import {motion} from 'framer-motion';
 
 const Herosection = () => {
   return (
-    <div className='font-poppins'>
+    <div className='font-poppins overflow-x-hidden'>
 
       {/* components from tailgrids */}
 
@@ -14,20 +15,39 @@ const Herosection = () => {
     <div class="-mx-4 flex flex-wrap">
       <div class="w-full px-4 lg:w-5/12">
         <div class="hero-content">
-          <h1
-              class="text-transparent bg-clip-text bg-gradient-to-r sm:text-8xl text-2xl from-blue-400 to-emerald-600 mb-3  font-bold leading-snug sm:text-[42px] lg:text-[40px] xl:text-[42px]">
-                  VapeHouse 
-                  <br />
-                  <span className='text-7xl'>
-                    Älvsjö bla vla vla vla bla bla bla
-                    </span>
+          
+          <motion.div 
+          variants={{
+            visible: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, y: 75 },
+          }}
+          initial= "hidden"
+          animate= "visible"
+          transition={{duration: 0.5, delay: 0.25}}
+        >
+          <h1 class="text-transparent bg-clip-text bg-gradient-to-r sm:text-8xl text-2xl from-blue-400 to-emerald-600 mb-3  font-bold leading-snug sm:text-[42px] lg:text-[40px] xl:text-[42px]">
+            VapeHouse 
+            <br />
+            <span className='sm:text-8xl text-6xl'>
+            Älvsjö bla vla vla vla
+            </span>
           </h1>
           <p class="text-white text-sm mb-8 max-w-[480px] sm:text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
             tristique urna in dui vehicula, et condimentum libero suscipit. 
             Nullam id aliquam metus. Integer laoreet nunc in ligula rhoncus, 
             </p>
-          
+          </motion.div>
+
+          <motion.div           v
+          ariants={{
+            visible: { opacity: 1, x: 0 },
+            hidden: { opacity: 0, x: -75 },
+          }}
+          initial= "hidden"
+          animate= "visible"
+          transition={{duration: 0.5, delay: 0.25}}
+        >
           <ul class="flex flex-wrap items-center">
                     <li>
                       <Link
@@ -46,6 +66,43 @@ const Herosection = () => {
                       </Link>
                     </li>
                   </ul>
+          </motion.div>
+
+          <motion.div
+                  variants={{
+                    visible: { opacity: 1 },
+                    hidden: { opacity: 0 },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.8, delay: 0.25 }}
+                  class="clients pt-16"
+                >
+                  <h6 class="text-white mb-2 flex items-center text-xs font-normal">
+                    Some Of Our Clients
+                    <span class="bg-body-color ml-2 inline-block h-[1px] w-8"></span>
+                  </h6>
+                  <div class="flex items-center">
+                    <div class="mr-4 w-full py-3">
+                      <img
+                        src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/ayroui.svg"
+                        alt="ayroui"
+                      />
+                    </div>
+                    <div class="mr-4 w-full py-3">
+                      <img
+                        src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/graygrids.svg"
+                        alt="graygrids"
+                      />
+                    </div>
+                    <div class="mr-4 w-full py-3">
+                      <img
+                        src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/uideck.svg"
+                        alt="uideck"
+                      />
+                    </div>
+                  </div>
+                </motion.div>
 
         </div>
       </div>
@@ -54,6 +111,16 @@ const Herosection = () => {
         <div class="lg:ml-auto lg:text-right">
           <div class="relative z-10 inline-block pt-11 lg:pt-0">
             
+          <motion.div
+                  variants={{
+                    visible: { opacity: 1, x: 0, filter: "blur(0px)" },
+                    hidden: { opacity: 0, x: 75, filter: "blur(5px)" },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.5, delay: 0.25 }}
+                  class="relative z-10 inline-block pt-11 lg:pt-0 mt-6"
+                >
           <img
               src={heroImage}
               alt="hero"
@@ -100,6 +167,7 @@ const Herosection = () => {
                 <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
               </svg>
             </span>
+            </motion.div>
           </div>
         </div>
       </div>
--- frontend/src/Components/Pages/HomePage/HomePage.jsx
+++ frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import Navbar from '../../Navigation/Navbar';
 import Herosection from '../../Herosection/Herosection';
-
+import Features from '../../../Features/Features';
 
 const HomePage = () => {
   return (
     <>
     <Navbar/> 
     <Herosection />
+    <Features />
     </>
   )
 }
