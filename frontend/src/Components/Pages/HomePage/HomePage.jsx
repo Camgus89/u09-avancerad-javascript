cc0018afc73c7c2a56520f2570c21a48475c54a7
Camgus89 <112941115+Camgus89@users.noreply.github.com>
2023-11-08 20:54:17 +0100

    fix

--- frontend/src/Components/Pages/HomePage/HomePage.jsx
+++ frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -1,29 +1,25 @@
-import Navbar from '../../Navigation/Navbar';
-import Herosection from '../../Herosection/Herosection';
+import Navbar from "../../Navigation/Navbar";
+import Herosection from "../../Herosection/Herosection";
 // import Features from '../../../Features/Features';
-import Products from '../Products/Products';
-import CallToAction from '../../CallToAction/CallToAction';
+import Products from "../Products/Products";
+import CallToAction from "../../CallToAction/CallToAction";
 import Review from "../../Review/Review";
 import Footer from "../../Footer/Footer";
-import SearchBar from '../../SearchBar/SearchBar';
-
+import SearchBar from "../../SearchBar/SearchBar";
 
 const HomePage = () => {
-
-
-
   return (
     <>
-    <Navbar/> 
-    <Herosection />
-    <SearchBar/>
-    {/* <Features /> */}
-    <Products/>
-    <CallToAction/>
-    <Review/>
-    <Footer/>
+      <Navbar />
+      <Herosection />
+      <SearchBar />
+      {/* <Features /> */}
+      <Products />
+      <CallToAction />
+      <Review />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
--- frontend/src/Components/Pages/Products/Products.jsx
+++ frontend/src/Components/Pages/Products/Products.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  
 
   useEffect(() => {
-    axios.get('http://localhost:8000/products')
+    axios
+      .get("http://localhost:8000/products")
       .then((response) => {
         setProducts(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
       });
   }, []);
 
@@ -20,8 +20,12 @@ const Products = () => {
     <div>
       <section className="bg-purple-50 pt-10 pb-10 lg:pt-[50px] lg:pb-20">
         <div>
-          <h2 className="text-3xl font-bold tracki text-center sm:text-5xl">Vape/E-cigaretter</h2>
-          <p className="max-w-3xl mx-auto mt-4 text-xl text-center">Ett brett sortiment av Stockholms bästa Vapes!</p>
+          <h2 className="text-3xl font-bold tracki text-center sm:text-5xl">
+            Vape/E-cigaretter
+          </h2>
+          <p className="max-w-3xl mx-auto mt-4 text-xl text-center">
+            Ett brett sortiment av Stockholms bästa Vapes!
+          </p>
         </div>
         <br />
         <div className="container mx-auto">
@@ -30,16 +34,15 @@ const Products = () => {
               <div key={product._id} className="w-full px-4 md:w-1/2 xl:w-1/3">
                 <div className="mb-10 overflow-hidden rounded-lg bg-white">
                   <img
-                     src={`/image/APPLE`}
+                    src={`/image/APPLE`}
                     alt={product.name}
                     className="w-full"
                   />
                   <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
                     <h3>
                       <Link
-                        to={`/productdetail/${product._id}`} // Länka till rätt produktens detaljsida
-                        className="text-dark hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
-                      >
+                        to={`/productdetail/${product._id}`} 
+                        className="text-dark hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
                         {product.name}
                       </Link>
                     </h3>
@@ -47,9 +50,8 @@ const Products = () => {
                       {product.price} kr
                     </p>
                     <Link
-                      to={`/productdetail/${product._id}`} // Uppdaterad länk till produktinformationssidan
-                      className="bg-purple-700 inline-flex items-center justify-center rounded-lg py-4 px-6 text-center text-base font-normal text-white hover:bg-purple-500 sm:px-10 lg:px-8 xl:px-10"
-                    >
+                      to={`/productdetail/${product._id}`} 
+                      className="bg-purple-700 inline-flex items-center justify-center rounded-lg py-4 px-6 text-center text-base font-normal text-white hover:bg-purple-500 sm:px-10 lg:px-8 xl:px-10">
                       Välj alternativ
                     </Link>
                   </div>
@@ -61,6 +63,6 @@ const Products = () => {
       </section>
     </div>
   );
-}
+};
 
 export default Products;
